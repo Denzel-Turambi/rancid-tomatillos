681a268401b180fdc2176882c4837f3a93f96817
Denzel Turambi <dturambi26@gmail.com>
2023-07-06 14:31:19 -0600

    feat: Add formatting and JSX to display card skeleton

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+function Card() {
+  return (
+    <div className='card'>
+      <img src='https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg' height='50px' width='30px'></img>
+      <h3>rating</h3>
+    </div>
+
+  )
+}
+
+export default Card;
