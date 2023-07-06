d35beb9ac314a08ccbeadc7d96484e6a78de5da4
Caitlin Cradick <caitlincradick@gmail.com>
2023-07-05 21:12:44 -0700

    Feat/add initial setup for App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,19 @@
 import './App.css';
 import {useState} from 'react';
 
+function App() {
+  const movieData = [{"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}]
+
+  const [movies, setMovies] = useState(movieData)
 
 
+return (
+  <div>
+  <h1>{movies[0]['movie'].title}</h1>
+  <img src='https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg'></img>
+  </div>
+)
 
+}
 
 export default App;
