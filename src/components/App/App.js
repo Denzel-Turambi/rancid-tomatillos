990da19b3bd99c89aa2712d54eca01c9270c3b0e
Denzel Turambi <dturambi26@gmail.com>
2023-07-06 14:33:24 -0600

    feat: Add import of other components and MovieContainer into JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
-import movieData from '../MovieData/MovieData.js';
+import movieData from '../MovieData/MovieData';
+import MovieContainer from '../MovieContainer/MovieContainer';
+import Card from '../Card/Card';
 import {useState} from 'react';
 
 function App() {
@@ -10,7 +12,9 @@ return (
   //use fragment instead of Div ?
   <div>
     <h1>{movies[0]['movie'].title}</h1>
-    <img src='https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg'></img>
+    {/* <img src='https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg'></img> */}
+    <MovieContainer />
+    
   </div>
 )
 }
