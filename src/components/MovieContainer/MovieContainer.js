a1abe0ac4746cb0ef304b6b7b97fb83717caf192
Denzel Turambi <dturambi26@gmail.com>
2023-07-06 14:32:19 -0600

    feat: Add MovieContainer formatting and JSX to contain all cards

---
+++ src/components/MovieContainer/MovieContainer.js
@@ -0,0 +1,12 @@
+import './MovieContainer.css';
+import Card from '../Card/Card';
+
+function MovieContainer() {
+  return (
+    <div className='movie-container'>
+      <Card />
+    </div>
+  )
+}
+
+export default MovieContainer
