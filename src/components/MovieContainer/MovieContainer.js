8734f6cca98280804abdfe928a2773a7965d1632
Denzel Turambi <dturambi26@gmail.com>
2023-07-06 18:40:22 -0600

    feat: Add card display rendering

--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -1,10 +1,22 @@
 import './MovieContainer.css';
 import Card from '../Card/Card';
 
-function MovieContainer() {
+function MovieContainer(props) {
+  const movieCards = props.movies.map(movie => {
+    return (
+      <Card 
+      title={movie.title}
+      img={movie.poster_path}
+      rating={movie.average_rating}
+      id={movie.id}
+      key={movie.id}
+      />
+    )
+  })
+
   return (
     <div className='movie-container'>
-      <Card />
+      {movieCards}
     </div>
   )
 }
