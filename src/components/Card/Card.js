17dacc54f7c38d2c0c4b2cfe7091f962fb53aa3e
Denzel Turambi <dturambi26@gmail.com>
2023-07-06 18:39:17 -0600

    feat: Add JSX to display movie cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,11 @@
 import './Card.css';
 
-function Card() {
+function Card({title, img, rating}) {
   return (
     <div className='card'>
-      <img src='https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg' height='50px' width='30px'></img>
-      <h3>rating</h3>
+      <img src={img} height='50px' width='30px'></img>
+      <h3>{title}</h3>
+      <h3>{rating}</h3>
     </div>
 
   )
