0934301effd7679453925000aa043447adeb57f1
Caitlin Cradick <caitlincradick@gmail.com>
2023-07-06 19:25:48 -0700

    Feat: add font and update CSS

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ function Card({title, img, rating}) {
     <div className='card'>
       <img src={img} height='50px' width='30px'></img>
       <h3>{title}</h3>
-      <h3>{rating}</h3>
+      <h3>{rating}/10</h3>
     </div>
 
   )
