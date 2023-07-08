4218225bc97aafe9a0b6cec4220bae730a3eb474
Caitlin Cradick <caitlincradick@gmail.com>
2023-07-07 17:26:02 -0700

    Feat/popup functionality fully functional

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,12 +7,29 @@ import {useState} from 'react';
 
 function App() {
   const [movies, setMovies] = useState(movieData)
+  //popup
+  const [showMovieDetail, setShowMovieDetail] = useState(false)
+
+  function showMovie(event) {
+    movies.forEach(movie => {
+      if(console.log(parseInt(event.target.id)) === console.log('MOVIE ID', movie.id)){
+        setShowMovieDetail(true)
+        console.log('CLICKED')
+       }
+    })
+  }
+
+  function exitShowMovie(){
+    setShowMovieDetail(false)
+  }
 
 return (
   //use fragment instead of Div ?
   <div>
     <h1>Rancid Tomatillos</h1>
-    <MovieContainer movies = {movies}/>
+    <section className='all-movies-display'>
+      <MovieContainer showMovieDetail= {showMovieDetail} showMovie= {showMovie} movies = {movies} exitShowMovie= {exitShowMovie}/>
+    </section>
   </div>
 )
 }
