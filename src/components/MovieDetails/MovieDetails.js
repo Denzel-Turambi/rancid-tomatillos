b2fb039b016cafc9eb1c28eedfa8fb4b7d4b0468
Denzel Turambi <dturambi26@gmail.com>
2023-07-15 13:31:17 -0600

    refactor: Refactor all files with correct indentation and remove unnecessary props

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,37 +1,35 @@
 import './MovieDetails.css';
 import { getSingleMovie } from '../../ApiCalls';
-import { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import { useParams, NavLink, Link} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useParams, Link } from 'react-router-dom';
 
 function MovieDetails({clearInput, setBarVisible}){  
 
-const id = useParams()
-  const [selectedMovie, setSelectedMovie] = useState({})
-  const [singleMovieError, setSingleMovieError] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
+const id = useParams();
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [singleMovieError, setSingleMovieError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-setBarVisible(false)
+setBarVisible(false);
 
 useEffect(() => {
   if(id.id){
-    setIsLoading(true)
-
+    setIsLoading(true);
   getSingleMovie(id.id)
     .then(data => setSelectedMovie(data.movie))
     .catch(err => {
       if(err.status === 500){
-        setSingleMovieError('Oops! There is a server error.')
+        setSingleMovieError('Oops! There is a server error.');
       } else {
-      setSingleMovieError(err)
+      setSingleMovieError(err);
       }
     })
     .finally(() => {
-      setIsLoading(false)
+      setIsLoading(false);
     })
   }
-}, [])
-
+}, []);
 
 if(singleMovieError){
   return <h1 className ="error-message" >{`${singleMovieError}. Please try again later.`}</h1>
@@ -42,7 +40,7 @@ if(singleMovieError){
       <div id={selectedMovie.id} className='movie-details'>
         <h2 className='title'>{selectedMovie.title}</h2>
         <Link to={"/"}>
-        <button className="details-btn"onClick={clearInput}> Back </button>
+          <button className="details-btn"onClick={clearInput}> Back </button>
         </Link>
         <p className="text-details">{selectedMovie.tagline}</p>
         <p className="text-details"> <strong>Average Rating: </strong> {selectedMovie.average_rating * 10}%</p>
@@ -50,25 +48,13 @@ if(singleMovieError){
         <p className="text-details"> <strong>ReleaseDate: </strong> {selectedMovie.release_date}</p>
         <p className="text-details"> <strong>Overview: </strong> {selectedMovie.overview}</p>
         <p className="text-details"> <strong>Genre(s): </strong> {selectedMovie.genres}</p>
-        {/* {selectedMovie.revenue && <p>Revenue: ${selectedMovie.revenue}</p> || <p>Revenue: Information not available</p>}
-        {selectedMovie.budget && <p>Budget: ${selectedMovie.budget}</p> || <p>Budget: Information not available</p>} */}
         <img className='movie-details-img' src={selectedMovie.backdrop_path} alt={selectedMovie.title}></img>
      </div>
     )
-}
+};
 
-export default MovieDetails
+export default MovieDetails;
 
 // MovieDetails.propTypes ={
 //   selectedMovieID: PropTypes.number.isRequired
 // }
-
-
-
-
-//page that you go to has to have access to the data
-//whatever your router is going to, you have to make sure the componnent has access to the data it needs 
-//router you will set it up with an element inside 
-//when you set up the routes the element is the component and you would pass the props through that component 
-//filter you would do use state 
-//router can set path with movies based on the ID
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,42 +1,39 @@
-import './SearchBar.css' 
+import './SearchBar.css'
 
-function SearchBar({search, searchFilter, clearInput, barVisible}){
-//pass search down as a prop with state 
+function SearchBar({ search, searchFilter, clearInput, barVisible }) {
+  if (!search && barVisible) {
+    return (
+      <div className='search-bar-container'>
+        <form>
+          <input
+            id='search'
+            type='text'
+            placeholder='Search for movies here!   🔍'
+            name={search}
+            value={search}
+            onChange={searchFilter}
+          />
+        </form>
+      </div>
+    )
+  } else if (search && barVisible) {
+    return (
+      <div className='search-bar-container-btn'>
 
-if(!search && barVisible){
-  return (
-  <div className='search-bar-container'>
-    
-    <form>
-      <input 
-      id='search'
-      type='text'
-      placeholder='Search for movies here!   🔍'
-      name= {search}
-      value={search}
-      onChange ={searchFilter}
-      />
-    </form>
-    </div>
-  )
-} else if (search && barVisible) {
-  return (
-    <div className='search-bar-container-btn'>
-     
-      <form className='form-btn'>
-        <input 
-        id='search-btn'
-        type='text'
-        placeholder='Search for movies here!   🔍'
-        name= {search}
-        value={search}
-        onChange ={searchFilter}
-        />
-      </form>
+        <form className='form-btn'>
+          <input
+            id='search-btn'
+            type='text'
+            placeholder='Search for movies here!   🔍'
+            name={search}
+            value={search}
+            onChange={searchFilter}
+          />
+        </form>
         <button className="clear-btn" onClick={clearInput}>clear input</button>
       </div>
     )
-}
-}
+  }
+};
 
-export default SearchBar
+export default SearchBar;
